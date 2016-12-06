b2fe3852af08234cf53f643b0d927cc4f7a17abe
makerdom <Dominik.Zians@student.ulg.ac.be>
2016-12-06 19:01:13 +0100

    Merge remote-tracking branch 'asassoye/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -1,19 +1,49 @@
 import pygame
 
-pygame.init()
-
-time = pygame.time.Clock()
-fini = False
-
-SCALE = 32
-LIMITES = [40, 20]
+"""
+"
+" Classe World, initialisation de pygame et affichage de la carte, joueurs
+"
+"""
 
 
 class World:
     def __init__(self):
-        self.surface = pygame.display.set_mode([LIMITES[0] * SCALE, LIMITES[1] * SCALE])
+        pygame.init()
+        self.time = pygame.time.Clock()
+        self.SCALE = 32
+        self.LIMITES = [40, 20]
+        self.surface = pygame.display.set_mode([self.LIMITES[0] * self.SCALE, self.LIMITES[1] * self.SCALE])
         self.ending = False
+        self.imageBlock = list()
+        self.level = list()
+        self.player = list()
+        self.carte = object()
         pygame.display.flip()
+
+    """
+    "
+    " Demarrage du jeu: initialisations des blocks joueurs et levels
+    "
+    """
+
+    def start(self):
+        self.initblocks()
+        self.initlevels()
+        self.initplayers()
+        while not self.ending:
+            self.eventlistener()
+            self.time.tick(60)
+
+        pygame.display.quit()
+        pygame.quit()
+        exit()
+
+    """
+    "
+    " Verifie les entrées au clavier
+    "
+    """
 
     def eventlistener(self):
         for evenement in pygame.event.get():
@@ -21,36 +51,115 @@ class World:
                 self.ending = True
             if evenement.type == pygame.KEYDOWN:
                 if evenement.key == pygame.K_z:
-                    player[0].deplacer('UP')
+                    self.player[0].deplacer('UP')
                 if evenement.key == pygame.K_d:
-                    player[0].deplacer('RIGHT')
+                    self.player[0].deplacer('RIGHT')
                 if evenement.key == pygame.K_s:
-                    player[0].deplacer('DOWN')
+                    self.player[0].deplacer('DOWN')
                 if evenement.key == pygame.K_q:
-                    player[0].deplacer('LEFT')
+                    self.player[0].deplacer('LEFT')
                 if evenement.key == pygame.K_UP:
-                    player[1].deplacer('UP')
+                    self.player[1].deplacer('UP')
                 if evenement.key == pygame.K_RIGHT:
-                    player[1].deplacer('RIGHT')
+                    self.player[1].deplacer('RIGHT')
                 if evenement.key == pygame.K_DOWN:
-                    player[1].deplacer('DOWN')
+                    self.player[1].deplacer('DOWN')
                 if evenement.key == pygame.K_LEFT:
-                    player[1].deplacer('LEFT')
+                    self.player[1].deplacer('LEFT')
 
-    def start(self):
-        while not self.ending:
-            self.eventlistener()
+    """
+    "
+    " initialisation des differents blocks
+    "
+    """
 
-            time.tick(60)
+    def initblocks(self):
+        self.imageBlock.append(ImageBlock("0", False))
+        self.imageBlock.append(ImageBlock("1", False))
+        self.imageBlock.append(ImageBlock("2", False))
+        self.imageBlock.append(ImageBlock("3", True))
 
-        pygame.display.quit()
-        pygame.quit()
-        exit()
+    """
+    "
+    " initialisation des joureurs
+    "
+    """
+
+    def initplayers(self):
+        self.player.append(Personnage('Andrew', [[0], [1, 1]], 2))
+        self.player.append(Personnage('Dominik', [[0], [38, 18]], 2))
+
+    """
+    "
+    " initalisations des diferents levels
+    "
+    """
+
+    def initlevels(self):
+        self.level.append(
+            [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+              3, 3, 3, 3, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+              2, 2, 2, 2, 3],
+             [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+              3, 3, 3, 3, 3]
+             ]
+        )
+
+        self.carte = Carte(self.level[0])
+        self.carte.affichecarte()
+
+
+"""
+"
+" Classe Carte, structure de la carte a l'affichage
+"
+"""
 
 
 class Carte:
     def __init__(self, elements):
         self.elements = elements
+
+    """
+    "
+    " Affichage de la carte
+    "
+    """
 
     def affichecarte(self):
         i = 0
@@ -59,15 +168,28 @@ class Carte:
             j = 0
 
             for element in lines:
-                imageBlock[element].dessine([j, i])
+                world.imageBlock[element].dessine([j, i])
                 j += 1
 
             i += 1
 
         pygame.display.flip()
 
+    """
+    "
+    " Return l'objet block se trouvant aux coordonnées données
+    "
+    """
+
     def quelblock(self, coordonnees):
-        return imageBlock[self.elements[coordonnees[1]][coordonnees[0]]]
+        return world.imageBlock[self.elements[coordonnees[1]][coordonnees[0]]]
+
+
+"""
+"
+" Class imageBlock: proprietés des blocks differents (obstacles ou pas etc)
+"
+"""
 
 
 class ImageBlock:
@@ -76,10 +198,23 @@ class ImageBlock:
         self.url = './imageBlock/' + nom + '.png'
         self.obstacle = obstacle
         self.surface = pygame.image.load(self.url).convert()
-        self.surface = pygame.transform.scale(self.surface, (SCALE, SCALE))
+        self.surface = pygame.transform.scale(self.surface, (world.SCALE, world.SCALE))
+
+    """
+    "
+    " Dessine le block sur la carte par rapport aux coordonnées données
+    "
+    """
 
     def dessine(self, coordonees):
-        world.surface.blit(self.surface, [coordonees[0] * SCALE, coordonees[1] * SCALE])
+        world.surface.blit(self.surface, [coordonees[0] * world.SCALE, coordonees[1] * world.SCALE])
+
+
+"""
+"
+" Class Personnage: Personnage et leur proprietés
+"
+"""
 
 
 class Personnage:
@@ -94,63 +229,134 @@ class Personnage:
             self.surface.append(
                 pygame.transform.scale(
                     pygame.image.load(self.url + pose + '.png'),
-                    (SCALE, SCALE)
+                    (world.SCALE, world.SCALE)
                 )
             )
+        self.dessine()
+
+    """
+    "
+    " Renvoi le type d'element (block) se trouvant a sa position
+    "
+    """
 
     def caseactuel(self):
-        return carte.elements[self.position[1][1]][self.position[1][0]]
+        return world.carte.elements[self.position[1][1]][self.position[1][0]]
+
+    """
+    "
+    " Recois la direction du eventlistener et se deplace si il peut se deplacer (verifie les obstacles, les limites de
+    " la fenetre et les autres personnages)
+    "
+    """
 
     def deplacer(self, evenement):
-        imageBlock[self.caseactuel()].dessine([self.position[1][0], self.position[1][1]])
+        world.imageBlock[self.caseactuel()].dessine([self.position[1][0], self.position[1][1]])
         if evenement == 'UP':
-            if not self.limite('UP') and not self.obstacle('UP'):
+            if not self.limite(evenement) and not self.obstacle(evenement) and not self.autrepersonne(evenement):
                 self.position[1][1] -= 1
                 self.pose = 0
         if evenement == 'RIGHT':
-            if not self.limite('RIGHT') and not self.obstacle('RIGHT'):
+            if not self.limite(evenement) and not self.obstacle(evenement) and not self.autrepersonne(evenement):
                 self.position[1][0] += 1
                 self.pose = 1
         if evenement == 'DOWN':
-            if not self.limite('DOWN') and not self.obstacle('DOWN'):
+            if not self.limite(evenement) and not self.obstacle(evenement) and not self.autrepersonne(evenement):
                 self.position[1][1] += 1
                 self.pose = 2
         if evenement == 'LEFT':
-            if not self.limite('LEFT') and not self.obstacle('LEFT'):
+            if not self.limite(evenement) and not self.obstacle(evenement) and not self.autrepersonne(evenement):
                 self.position[1][0] -= 1
                 self.pose = 3
         self.dessine()
+
+    """
+    "
+    " Verifie si il depace une limite en se deplacant dans $direction
+    "
+    """
 
     def limite(self, direction):
         if direction == 'UP':
             return self.position[1][1] == 0
         if direction == 'RIGHT':
-            return self.position[1][0] == LIMITES[0] - 1
+            return self.position[1][0] == world.LIMITES[0] - 1
         if direction == 'DOWN':
-            return self.position[1][1] == LIMITES[1] - 1
+            return self.position[1][1] == world.LIMITES[1] - 1
         if direction == 'LEFT':
             return self.position[1][0] == 0
 
+    """
+    "
+    " Verifie si en se deplacant on se deplace sur un obstacle par rapport a $direction
+    "
+    """
+
     def obstacle(self, direction):
         if direction == 'UP':
-            return carte.quelblock([self.position[1][0], self.position[1][1] - 1]).obstacle
+            return world.carte.quelblock([self.position[1][0], self.position[1][1] - 1]).obstacle
         if direction == 'RIGHT':
-            return carte.quelblock([self.position[1][0] + 1, self.position[1][1]]).obstacle
+            return world.carte.quelblock([self.position[1][0] + 1, self.position[1][1]]).obstacle
         if direction == 'DOWN':
-            return carte.quelblock([self.position[1][0], self.position[1][1] + 1]).obstacle
+            return world.carte.quelblock([self.position[1][0], self.position[1][1] + 1]).obstacle
         if direction == 'LEFT':
-            return carte.quelblock([self.position[1][0] - 1, self.position[1][1]]).obstacle
+            return world.carte.quelblock([self.position[1][0] - 1, self.position[1][1]]).obstacle
+
+    """
+    "
+    " Verifie si en se deplacant on se deplace sur un autre personnage par rapport a $direction
+    "
+    """
+
+    def autrepersonne(self, direction):
+        if direction == 'UP':
+            if self.nom == 'Andrew':
+                return self.position[1][1] == world.player[1].position[1][1] + 1 and self.position[1][0] == \
+                                                                                     world.player[1].position[1][0]
+            else:
+                return self.position[1][1] == world.player[0].position[1][1] + 1 and self.position[1][0] == \
+                                                                                     world.player[0].position[1][0]
+        if direction == 'RIGHT':
+            if self.nom == 'Andrew':
+                return self.position[1][0] == world.player[1].position[1][0] - 1 and self.position[1][1] == \
+                                                                                     world.player[1].position[1][1]
+            else:
+                return self.position[1][0] == world.player[0].position[1][0] - 1 and self.position[1][1] == \
+                                                                                     world.player[0].position[1][1]
+        if direction == 'DOWN':
+            if self.nom == 'Andrew':
+                return self.position[1][1] == world.player[1].position[1][1] - 1 and self.position[1][0] == \
+                                                                                     world.player[1].position[1][0]
+            else:
+                return self.position[1][1] == world.player[0].position[1][1] - 1 and self.position[1][0] == \
+                                                                                     world.player[0].position[1][0]
+        if direction == 'LEFT':
+            if self.nom == 'Andrew':
+                return self.position[1][0] == world.player[1].position[1][0] + 1 and self.position[1][1] == \
+                                                                                     world.player[1].position[1][1]
+            else:
+                return self.position[1][0] == world.player[0].position[1][0] + 1 and self.position[1][1] == \
+                                                                                     world.player[0].position[1][1]
+
+    """
+    "
+    " Affiche le personnage par rapport a sa postion
+    "
+    """
 
     def dessine(self):
-        world.surface.blit(self.surface[self.pose], [self.position[1][0] * SCALE, self.position[1][1] * SCALE])
+        world.surface.blit(self.surface[self.pose],
+                           [self.position[1][0] * world.SCALE, self.position[1][1] * world.SCALE])
         pygame.display.flip()
 
 
-player = list()
-player.append(Personnage('Andrew', [[0], [1, 1]], 2))
-player.append(Personnage('Dominik', [[0], [38, 18]], 2))
-
+"""
+"
+" Initialise le monde et demarre
+"
+"""
 world = World()
+<<<<<<< HEAD
 
 imageBlock = list()
 imageBlock.append(ImageBlock("0", False))
@@ -187,4 +393,6 @@ carte.affichecarte()
 player[0].dessine()
 player[1].dessine()
 
+=======
+>>>>>>> asassoye/master
 world.start()
